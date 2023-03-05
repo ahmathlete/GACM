65705be2b7770c10b1313cff8699a19cd3d60339
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2023-03-05 11:16:44 +0100

    clean

---
+++ src/CONSTANTS.f90
@@ -0,0 +1,7 @@
+module GACM_CONSTANTS
+    use, intrinsic :: iso_c_binding
+    implicit none
+    real(c_double), parameter :: PlanetGrav = 9.80665
+    real(c_double), parameter :: PlanetRD = 287.058
+
+end module GACM_CONSTANTS
--- src/PRESSURE.f90
+++ src/PRESSURE.f90
@@ -1,5 +1,6 @@
 module GACM_PRESSURE
    use, intrinsic :: iso_c_binding
+   use GACM_CONSTANTS
    implicit none
    private
    public :: GACM_PRESSURE_HYBRID_SIGMA
@@ -27,7 +28,34 @@ contains
       end do
 
    end subroutine GACM_PRESSURE_HYBRID_SIGMA
-   subroutine GACM_PRESSURE_HYBRID_SIGMA_HEIGHT() bind(C, name="GACM_PRESSURE_HYBRID_SIGMA_HEIGHT_")
+   subroutine GACM_PRESSURE_HYBRID_SIGMA_HEIGHT(nlat, nlon, nlevel, ntime, &
+                                                orography, surface_pressure, a, b, p0, &
+                                                TEMP_4D, PRESSURE_4D) bind(C, &
+                                                                           name="GACM_PRESSURE_HYBRID_SIGMA_HEIGHT_")
+
+      integer(c_int), value                                             :: nlat, nlon, nlevel, ntime
+      real(c_double)                                                    :: orography(nlat, nlon)
+      real(c_double)                                                    :: surface_pressure(nlat, nlon, ntime)
+      real(c_double)                                                    :: a(nlevel), b(nlevel), p0
+      real(c_double)                                                    :: TEMP_4D(nlat, nlon, nlevel, ntime)
+      real(c_double), intent(out)                                       :: PRESSURE_4D(nlat, nlon, nlevel, ntime)
+      integer(c_int)                                                    :: i, j, l, t
+      real(c_double)                                                    :: HEIGHT, CON_VAR
+
+      CON_VAR = PlanetGrav/PlanetRD
+
+      ! can be parralised with omp
+      do t = 1, ntime
+         do l = 1, nlevel
+            do j = 1, nlon
+               do i = 1, nlat
+                  HEIGHT = a(l) + b(l)*orography(i, j)
+                  PRESSURE_4D(i, j, l, t) = surface_pressure(i, j, t)*exp(-1*(CON_VAR/TEMP_4D(i, j, l, t)* &
+                                                                              (HEIGHT - orography(i, j))))
+               end do
+            end do
+         end do
+      end do
 
    end subroutine GACM_PRESSURE_HYBRID_SIGMA_HEIGHT
 end module GACM_PRESSURE
